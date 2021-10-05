4af17bc7aaceeb7d78bb14bbb36bfa9b3a79f401
nicostrinna <nicostrinna@hotmail.com>
2021-10-05 18:48:31 -0300

    primer comit segunda clase

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let numUsuario = parseInt(prompt('ingresar un numero del 1 al 10'));
+if(numUsuario <=10) {
+    alert("¡¡¡Excelente!!!")
+}
+else{
+    alert('ERROR "no cumples con la consigna"')
+};
+
+let saluInigreso = prompt('Saluda con un "Hola"');
+if(saluInigreso == "hola"){
+    console.log("¡¡¡Excelente!!!");
+}
+else{
+    console.log("Error");
+}
+
+
+
